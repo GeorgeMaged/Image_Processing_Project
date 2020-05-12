0cce01d49319b0924f11121181f26b00f9c9a427
Abanoub Entesar <beboentesar@gmail.com>
2020-05-12 17:13:21 +0200

    Extract text

--- Dawa2i.py
+++ Dawa2i.py
@@ -53,3 +53,64 @@ def play_audio(output):
     my_object.save("welcome.mp3")
     os.system("start welcome.mp3")
 
+
+# Function to extract text from image
+def extract_text():
+
+    # Extract all the text from the image
+    extracted_text = pytesseract.image_to_string(image)
+
+    # Convert the text into list
+    list_extracted_text = extracted_text.split()
+    # print(list_extracted_text)
+
+    # Connect to the database
+    conn = sqlite3.connect('Medicine_DB')
+
+    # Create cursor
+    c = conn.cursor()
+
+    # Fetch all the records saved in the database
+    c.execute("SELECT * FROM Medicine ")
+    records = c.fetchall()
+    # print(records)
+
+    # Commit changes
+    conn.commit()
+
+    # Close connection
+    conn.close()
+
+    # Check if name of medicine matches any medicine name in the database
+    # If yes it passes its name as well as the times it should be taken to the function play_audio
+    # If not an audio is played that says drug not found and opens a window to add it to the database
+    names = []
+    times = []
+    global to_speak
+    to_speak = ''
+    for record in records:
+        names.append(record[0])
+        times.append(record)
+    # print(names)
+    global flag
+    flag = 0
+    for element in list_extracted_text:
+        for one_name in names:
+            if element == one_name:
+                flag = 1
+                to_speak += element
+                for record in records:
+                    if record[0] == element:
+                        to_speak += record[1]
+
+    # If the drug is found in the database
+    if flag == 1:
+        print(to_speak)
+        play_audio(to_speak)
+        flag = 0
+    # If the drug is not found in the database
+    elif flag == 0:
+        to_speak = "Drug Name Not Found Please Select Add New Drug"
+        play_audio(to_speak)
+        open_new_drug_window()
+
